b429096ef491359846b099f674903bf44d6423dd
Tylor Shin <shincode@icloud.com>
2017-05-12 19:08:26 +0900

    Added multipart/parser

--- src/handlers/uploadImage/index.ts
+++ src/handlers/uploadImage/index.ts
@@ -1,46 +1,50 @@
-import * as fs from "fs";
-import * as GraphicMagick from "gm";
+import * as Busboy from "busboy";
 
 const handler: AWSLambda.ProxyHandler = async (event, context, _callback) => {
-  const gm = GraphicMagick.subClass({ imageMagick: true });
+  // const gm = GraphicMagick.subClass({ imageMagick: true });
+  const buffer = new Buffer(event.body || "", "base64");
+  console.log(buffer.toString())  ;
 
-  const buffer = Buffer.from(event.body || "", "base64");
+  const contentType = event.headers["Content-Type"] || event.headers["content-type"];
+  console.log(contentType);
+  const busboy = new Busboy({ headers: { "content-type": contentType }});
 
-  fs.writeFileSync("/tmp/original.png", buffer, "base64");
-  fs.readFile("/tmp/original.png", (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(data);
-      console.log(data.toString("base64"));
-    }
-  });
-
-  if (event.body) {
-    await new Promise((resolve, reject) => {
-      gm("/tmp/original.png")
-        .write("/tmp/result", (err: Error, res: any) => {
-          if (err) {
-            console.log(err);
-            reject(err);
-          } else {
-            console.log(res);
-            resolve();
-          }
-        });
+  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+    console.log(
+      "File [" + fieldname + "]: filename: " + filename + ", encoding: " + encoding + ", mimetype: " + mimetype,
+    );
+    file.on("data", (data: any) => {
+      console.log("File [" + fieldname + "] got " + data.length + " bytes");
     });
-  }
-
-  // return HTTP result
-  context.done(undefined, {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials" : true,
-    },
-    body: JSON.stringify("hello world"),
-    isBase64Encoded: true,
+    file.on("end", () => {
+      console.log("File [" + fieldname + "] Finished");
+    });
   });
+  busboy.on("field", (fieldname, val) => {
+    console.log("Field [" + fieldname + "]: value: " + (val));
+  });
+  busboy.on("finish", () => {
+    console.log("Done parsing form!");
+
+    // return HTTP result
+    context.done(undefined, {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS, POST",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+      body: JSON.stringify("hello world"),
+    });
+  });
+  busboy.on("error", (err: Error) => {
+    console.log("failed", err);
+  });
+
+  console.log(event.body);
+
+  busboy.end(buffer.toString());
 };
 
 export default handler;
