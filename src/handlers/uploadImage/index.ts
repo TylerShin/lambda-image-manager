fc28dd5e807555058eb1c09e11f390b2748ef68c
Tylor Shin <shincode@icloud.com>
2017-05-14 17:43:12 +0900

    Added S3 options. but upload doesn't work properly yet

--- src/handlers/uploadImage/index.ts
+++ src/handlers/uploadImage/index.ts
@@ -1,31 +1,61 @@
 import * as Busboy from "busboy";
+import * as AWS from "aws-sdk";
 
 const handler: AWSLambda.ProxyHandler = async (event, context, _callback) => {
-  // const gm = GraphicMagick.subClass({ imageMagick: true });
   const buffer = new Buffer(event.body || "", "base64");
-  console.log(buffer.toString())  ;
-
   const contentType = event.headers["Content-Type"] || event.headers["content-type"];
-  console.log(contentType);
   const busboy = new Busboy({ headers: { "content-type": contentType }});
 
+  const s3 = new AWS.S3();
+
+  const promiseArr: Array<Promise<any>> = [];
   busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
     console.log(
       "File [" + fieldname + "]: filename: " + filename + ", encoding: " + encoding + ", mimetype: " + mimetype,
     );
+
     file.on("data", (data: any) => {
       console.log("File [" + fieldname + "] got " + data.length + " bytes");
+
+      const date = new Date();
+      const fileId = date.toISOString();
+
+
+      // console.log("UTF8", data.toString("utf8"));
+      // console.log("ASCII", data.toString("ascii"));
+      // console.log("BASE64", data.toString("base64"));
+
+      promiseArr.push(new Promise((resolve, reject) => {
+        s3.upload({
+          Bucket: process.env.S3_BUCKET_NAME,
+          Key: `${process.env.S3_DEST_PREFIX}/${fileId}/${filename}`,
+          ContentType: mimetype,
+          Body: data,
+        }, undefined, (err, result) => {
+          if (err) {
+            console.log(err);
+            reject(err);
+          } else {
+            console.log(`S3 upload success! result === ${JSON.stringify(result)}`);
+            resolve();
+          }
+        });
+      }));
     });
+
     file.on("end", () => {
       console.log("File [" + fieldname + "] Finished");
     });
+
   });
   busboy.on("field", (fieldname, val) => {
     console.log("Field [" + fieldname + "]: value: " + (val));
   });
-  busboy.on("finish", () => {
+
+  busboy.on("finish", async () => {
     console.log("Done parsing form!");
 
+    await Promise.all(promiseArr);
     // return HTTP result
     context.done(undefined, {
       statusCode: 200,
@@ -35,14 +65,16 @@ const handler: AWSLambda.ProxyHandler = async (event, context, _callback) => {
         "Access-Control-Allow-Methods": "OPTIONS, POST",
         "Access-Control-Allow-Headers": "Content-Type",
       },
-      body: JSON.stringify("hello world"),
+      body: JSON.stringify(process.env),
     });
   });
+
   busboy.on("error", (err: Error) => {
     console.log("failed", err);
   });
 
-  console.log(event.body);
+
+  console.log(buffer.toString());
 
   busboy.end(buffer.toString());
 };
