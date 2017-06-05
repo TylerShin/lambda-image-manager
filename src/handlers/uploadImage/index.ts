a854e02c0b78146447a14592599b9522f493ad0e
Tylor Shin <shincode@icloud.com>
2017-06-05 19:07:34 +0900

    WIP to make upload image

--- src/handlers/uploadImage/index.ts
+++ src/handlers/uploadImage/index.ts
@@ -1,61 +1,16 @@
-import * as Busboy from "busboy";
-import * as AWS from "aws-sdk";
+// import * as Busboy from "busboy";
+// import * as AWS from "aws-sdk";
+import S3Manager from "../../helpers/s3Manager";
 
 const handler: AWSLambda.ProxyHandler = async (event, context, _callback) => {
-  const buffer = new Buffer(event.body || "", "base64");
-  const contentType = event.headers["Content-Type"] || event.headers["content-type"];
-  const busboy = new Busboy({ headers: { "content-type": contentType }});
 
-  const s3 = new AWS.S3();
+  console.log(event.body, "event. body");
+  const buffer = new Buffer(event.body as string, "base64");
+  const decodedString = buffer.toString("ascii");
+  console.log(decodedString, "decoded body");
 
-  const promiseArr: Array<Promise<any>> = [];
-  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-    console.log(
-      "File [" + fieldname + "]: filename: " + filename + ", encoding: " + encoding + ", mimetype: " + mimetype,
-    );
-
-    file.on("data", (data: any) => {
-      console.log("File [" + fieldname + "] got " + data.length + " bytes");
-
-      const date = new Date();
-      const fileId = date.toISOString();
-
-
-      // console.log("UTF8", data.toString("utf8"));
-      // console.log("ASCII", data.toString("ascii"));
-      // console.log("BASE64", data.toString("base64"));
-
-      promiseArr.push(new Promise((resolve, reject) => {
-        s3.upload({
-          Bucket: process.env.S3_BUCKET_NAME,
-          Key: `${process.env.S3_DEST_PREFIX}/${fileId}/${filename}`,
-          ContentType: mimetype,
-          Body: data,
-        }, undefined, (err, result) => {
-          if (err) {
-            console.log(err);
-            reject(err);
-          } else {
-            console.log(`S3 upload success! result === ${JSON.stringify(result)}`);
-            resolve();
-          }
-        });
-      }));
-    });
-
-    file.on("end", () => {
-      console.log("File [" + fieldname + "] Finished");
-    });
-
-  });
-  busboy.on("field", (fieldname, val) => {
-    console.log("Field [" + fieldname + "]: value: " + (val));
-  });
-
-  busboy.on("finish", async () => {
-    console.log("Done parsing form!");
-
-    await Promise.all(promiseArr);
+  try {
+    await S3Manager.uploadFile(buffer, "hosigi", "chicken");
     // return HTTP result
     context.done(undefined, {
       statusCode: 200,
@@ -65,18 +20,21 @@ const handler: AWSLambda.ProxyHandler = async (event, context, _callback) => {
         "Access-Control-Allow-Methods": "OPTIONS, POST",
         "Access-Control-Allow-Headers": "Content-Type",
       },
-      body: JSON.stringify(process.env),
+      body: JSON.stringify(decodedString),
     });
-  });
-
-  busboy.on("error", (err: Error) => {
-    console.log("failed", err);
-  });
-
-
-  console.log(buffer.toString());
-
-  busboy.end(buffer.toString());
+  } catch (_err) {
+    // return HTTP result
+    context.done(undefined, {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS, POST",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+      body: JSON.stringify(decodedString),
+    });
+  }
 };
 
 export default handler;
--- src/helpers/s3Manager.ts
+++ src/helpers/s3Manager.ts
@@ -3,6 +3,24 @@ import * as AWS from "aws-sdk";
 class S3Manager {
   private s3 = new AWS.S3();
 
+  public uploadFile(buffer: Buffer, fileId: string, fileName: string) {
+    return new Promise((resolve, reject) => {
+      this.s3.upload({
+        Body: buffer,
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: `${process.env.S3_DEST_PREFIX}/${fileId}/${fileName}`,
+      }, (err: Error, data: any) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          console.log(data);
+          resolve(data);
+        }
+      });
+    });
+  }
+
   public checkFileExist(fileId: string, fileName: string) {
     return new Promise((resolve, _reject) => {
       this.s3.headObject({
